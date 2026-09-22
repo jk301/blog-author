d49fbb0c9841642f7878faf9ceb0e5b24e6e596e
junaid <jk7948677@gmail.com>
2026-09-22 15:20:33 +0530

    switch to select diff category

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,6 +6,7 @@ import '../styles/home.css'
 function Home({ logged }) {
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(true)
+    const [filter, setFilter] = useState('all')
 
     useEffect(() => {
         async function fetchPosts() {
@@ -34,19 +35,44 @@ function Home({ logged }) {
 
     if (loading) return <div className="status-msg"><p>Fetching posts..</p></div>
     if (!loading && posts.length === 0) {
-         return <div className="status-msg"><p>No posts available</p></div>
+        return <div className="status-msg"><p>No posts yet.</p></div>
     }
+
+    const filPosts = posts.filter( post => {
+        if (filter === 'unpub') return !post.isPub 
+        if (filter === 'pub') return post.isPub
+        return true
+    })
 
   return (
     <div className="post-container">
-        {!logged && <h1>Sign up & login to see/edit your posts! </h1>}
-        { posts.map( post => (
-            <div key={post.id} className="post-div" >
-                <Link to={`/posts/${post.id}`}>
-                    <h2>{ post.title }</h2>
-                </Link>
-            </div>
-        ))}
+        {!logged && <h1>Sign up & login to create/see/edit your posts! </h1>}
+        {logged 
+            && <div>
+                <div className="post-select">
+                    <h3 
+                        onClick={() => setFilter('all')} 
+                        className={filter === 'all' ? 'active-tab' : ''}
+                    >All posts</h3>
+                    <h3 
+                        onClick={() => setFilter('unpub')} 
+                        className={filter === 'unpub' ? 'active-tab' : ''}
+                    >Unpublished</h3>
+
+                    <h3 
+                        onClick={() => setFilter('pub')} 
+                        className={filter === 'pub' ? 'active-tab' : ''}
+                    >Published</h3>
+                </div>
+                { filPosts.map( post => (
+                    <div key={post.id} className="post-div" >
+                        <Link to={`/posts/${post.id}`}>
+                            <h2>{ post.title }</h2>
+                        </Link>
+                    </div>
+                ))}
+            </div> 
+        }      
     </div>
   )
 }
